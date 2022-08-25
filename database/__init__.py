4977fba2e99c494d79a514ef425bed722a878763
anlcnt <derrechtloz@gmail.com>
2022-08-25 18:30:56 +0300

    Декоратор отлова ошибки

--- database/__init__.py
+++ database/__init__.py
@@ -3,6 +3,15 @@ from settings import MySQLSettings
 import logging
 
 
+def exception_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Error as e:
+            logging.error(e)
+    return wrapper
+
+
 class MySQLClientMixin:
     # TODO: Создание базы данных при её отсутствии
     def connect(self):
